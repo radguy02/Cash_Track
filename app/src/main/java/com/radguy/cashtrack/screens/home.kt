c3122179c10be3f5071bfbf0e3db9aa125f0983a
RADGuy <anishraj365@gmail.com>
2025-03-26 02:24:58 +0530

    Navigation Dependencies

--- app/src/main/java/com/radguy/cashtrack/screens/home.kt
+++ app/src/main/java/com/radguy/cashtrack/screens/home.kt
@@ -1,2 +1,10 @@
 package com.radguy.cashtrack.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun screen_home() {
+
+}
